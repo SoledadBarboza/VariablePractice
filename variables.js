ab106930706482f43294fa1df49e843986df792a
Sole <barboza.soledad@gmail.com>
2023-06-07 14:43:29 -0300

    Different values for strings and several console.log statements

--- variables.js
+++ variables.js
@@ -1,12 +1,29 @@
 // STRINGS
 var username = "María"
+username = "María Julia"
+//Print the message on the console
+console.log(username);
+
 var email = "maria@email.com"
+email = "mariajulia@email.com"
+//Print the message on the console
+console.log(email);
 
 // NUMBERS
 var age = 30
+//Print the message on the console
+console.log(age);
+
 var userId = 2563
+//Print the message on the console
+console.log(userId);
 
-// BOOLEANS
+
+// BOOLEANOS
 var livesInArgentine = true
-var hasPets = true
+//Print the message on the console
+console.log(livesInArgentine);
 
+var hasPets = true
+//Print the message on the console
+console.log(hasPet);
